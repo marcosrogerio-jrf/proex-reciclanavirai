41d9569ad180ef4dc3fbfc9a989fc1b576cc103c
marcosrogerio-jrf <marcosrogerio.jrf@gmail.com>
2020-05-12 17:02:57 -0400

    Criação do projeto

---
+++ src/main/java/com/reciclanavirai/web/ReciclanaviraiApplication.java
@@ -0,0 +1,13 @@
+package com.reciclanavirai.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclanaviraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclanaviraiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reciclanavirai/web/ReciclanaviraiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reciclanavirai.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclanaviraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

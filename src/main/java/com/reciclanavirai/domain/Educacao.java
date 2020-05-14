6211a087320fd6d8f0f46cf75d279d0fd265d013
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2020-05-13 23:05:01 -0400

    Correções na classe Educação

--- src/main/java/com/reciclanavirai/domain/Educacao.java
+++ src/main/java/com/reciclanavirai/domain/Educacao.java
@@ -1,5 +1,33 @@
 package com.reciclanavirai.domain;
 
-public class Educacao {
+import java.io.File;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+
+
+@Entity @SuppressWarnings("serial")
+public class Educacao extends AbstractEntity<Long>{
+	
+	@Column(nullable = false, length = 70)
+	private String titulo;
+	
+	@Column(nullable = false, length = 120)
+	private String materia;
+	
+	@Column(nullable = false, length = 700)
+	private String autoria;
+	
+	@Column(nullable = true)
+	private File fotos;
+	
+	@ManyToOne @JoinColumn(name = "id_gestor_fk")
+	private Gestor gestor;
+	
+	
+	
 }

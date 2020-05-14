618edc3a50e18e93e82bd6d52448ab13b70940f4
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2020-05-13 22:14:07 -0400

    implementação da classe Municipe

--- src/main/java/com/reciclanavirai/domain/Municipe.java
+++ src/main/java/com/reciclanavirai/domain/Municipe.java
@@ -1,5 +1,26 @@
 package com.reciclanavirai.domain;
 
-public class Municipe {
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+
+@Entity @SuppressWarnings("serial")
+public class Municipe extends Usuario{
+	
+	@ManyToOne @JoinColumn(name ="id_bairro_fk")
+	private Bairro bairro;
+
+	//GET E SET
+	
+	public Bairro getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(Bairro bairro) {
+		this.bairro = bairro;
+	}
+	
+	
+	
 }

ece0ae07332a08993806b52cccd7cba17dc9b552
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2020-05-13 22:51:26 -0400

    Correções na classe Gestor

--- src/main/java/com/reciclanavirai/domain/Gestor.java
+++ src/main/java/com/reciclanavirai/domain/Gestor.java
@@ -1,7 +1,10 @@
 package com.reciclanavirai.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 @Entity @SuppressWarnings("serial")
 public class Gestor extends Usuario{
@@ -9,8 +12,11 @@ public class Gestor extends Usuario{
 	@Column(nullable = false, length = 80)
 	private String cargo;
 	
+	@OneToMany(mappedBy = "gestor")
+	private List<ProgramaAcao> programasAcoes;
+	
 	//GET E SET
-
+	
 	public String getCargo() {
 		return cargo;
 	}
@@ -19,5 +25,12 @@ public class Gestor extends Usuario{
 		this.cargo = cargo;
 	}
 	
+	public List<ProgramaAcao> getProgramasAcoes() {
+		return programasAcoes;
+	}
+
+	public void setProgramasAcoes(List<ProgramaAcao> programasAcoes) {
+		this.programasAcoes = programasAcoes;
+	}	
 	
 }

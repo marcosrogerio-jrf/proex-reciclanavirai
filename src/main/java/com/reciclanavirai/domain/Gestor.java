d0727ad553555c45603cc0a901f912da72fbe4f1
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2020-05-13 22:19:35 -0400

    Correções na classe Gestor

--- src/main/java/com/reciclanavirai/domain/Gestor.java
+++ src/main/java/com/reciclanavirai/domain/Gestor.java
@@ -1,10 +1,12 @@
 package com.reciclanavirai.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity @SuppressWarnings("serial")
 public class Gestor extends Usuario{
 	
+	@Column(nullable = false, length = 80)
 	private String cargo;
 	
 	//GET E SET

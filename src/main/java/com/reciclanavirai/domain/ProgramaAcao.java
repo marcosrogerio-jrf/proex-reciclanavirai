9b452935e570dea6fda411e288eadeb1a20b7de1
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2020-05-13 23:06:15 -0400

    Classe ProgramaAcao implementada

--- src/main/java/com/reciclanavirai/domain/ProgramaAcao.java
+++ src/main/java/com/reciclanavirai/domain/ProgramaAcao.java
@@ -1,14 +1,10 @@
 package com.reciclanavirai.domain;
 
 import java.sql.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-
-
 
 
 @Entity @SuppressWarnings("serial")
@@ -28,6 +24,52 @@ public class ProgramaAcao extends AbstractEntity<Long>{
 	
 	@ManyToOne @JoinColumn(name = "id_gestor_fk")
 	private Gestor gestor;
+	
+	//GET E SET
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Bairro getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(Bairro bairro) {
+		this.bairro = bairro;
+	}
+
+	public Gestor getGestor() {
+		return gestor;
+	}
+
+	public void setGestor(Gestor gestor) {
+		this.gestor = gestor;
+	}
+	
+	
+	
+	
 	
 	
 }
